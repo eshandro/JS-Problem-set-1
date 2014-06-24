c980e9c5f029f242f0a9a9f9e07f02d6c7131944
Erik Shandrowski <erik843@gmail.com>
2014-06-24 17:57:25 -0600

    Problem 3 done

--- main.js
+++ main.js
@@ -12,7 +12,24 @@ var letterCapitalize = function(string) {
 // letterCapitalize("word1 word2 word3 word4");
 
 var wordCount = function(string) {
-	console.log((string.split(" ")).length);
 	return (string.split(" ")).length;
 }
-wordCount("word1 word2");
+// wordCount("word1 word2");
+
+var primeTime = function(number) {
+	if (number === 2 || number === 3) {
+		return true;
+	}	
+	if (number < 2 || number % 2 === 0 || number % 3 === 0) {
+		return false
+	}
+	
+	for(i=5; i <= number/2; i+=2) {
+		if (number % i === 0) {
+			return false
+		}
+	}
+	return true
+}
+
+// console.log(primeTime(99839));
